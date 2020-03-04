477a724a76d46355fdbebd4cb22fbe3eebbc5d71
szn <>
2020-03-04 19:15:43 +0800

    add directory check

--- SQLite3Helper/Editor/SQLite3/SQLite3BuildProcess.cs
+++ SQLite3Helper/Editor/SQLite3/SQLite3BuildProcess.cs
@@ -6,44 +6,51 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEngine;
 
-public class SQLite3BuildProcess : IPreprocessBuild
+namespace Szn.Framework.Editor.SQLite3Creator
 {
-    public int callbackOrder { get; private set; }
-
-    public void OnPreprocessBuild(BuildTarget InTarget, string InPath)
+    public class SQLite3BuildProcess : IPreprocessBuild
     {
-        string streamingAssetsPath = Application.streamingAssetsPath;
-        List<FileInfo> fileInfos = FileTools.GetAllFileInfos(streamingAssetsPath);
-        if(null == fileInfos) return;
-        List<FileInfo> dbFileInfos = new List<FileInfo>();
-        int count = fileInfos.Count;
-        for (int i = 0; i < count; i++)
-        {
-            if (fileInfos[i].Extension == ".db")
-            {
-                dbFileInfos.Add(fileInfos[i]);
-            }
-        }
+        public int callbackOrder { get; private set; }
 
-        SQLite3Data data = Resources.Load<SQLite3Data>("Sqlite3Data");
-        string dataPath = AssetDatabase.GetAssetPath(data);
-        data = ScriptableObject.CreateInstance<SQLite3Data>();
-        int dbCount = dbFileInfos.Count;
-        data.AllData = new List<SQLite3SingleData>(dbCount);
-        for (int i = 0; i < dbCount; i++)
+        public void OnPreprocessBuild(BuildTarget InTarget, string InPath)
         {
-            SQLite3SingleData singleData = new SQLite3SingleData
+            string streamingAssetsPath = Application.streamingAssetsPath;
+            if (!Directory.Exists(streamingAssetsPath)) return;
+            List<FileInfo> fileInfos = FileTools.GetAllFileInfos(streamingAssetsPath);
+            if (null == fileInfos) return;
+            List<FileInfo> dbFileInfos = new List<FileInfo>();
+            int count = fileInfos.Count;
+            for (int i = 0; i < count; i++)
             {
-                Name =  dbFileInfos[i].Name,
-                LocalName = MD5Tools.GetStringMd5(dbFileInfos[i].Name),
-                Md5 = MD5Tools.GetFileMd5(dbFileInfos[i].FullName)
-            };
-            string dirPath = dbFileInfos[i].DirectoryName;
-            singleData.Directory = string.IsNullOrEmpty(dirPath) || dirPath == streamingAssetsPath ? string.Empty : dirPath.Replace('\\', '/').Replace(streamingAssetsPath, string.Empty);
-            
-            data.AllData.Add(singleData);
+                if (fileInfos[i].Extension == ".db")
+                {
+                    dbFileInfos.Add(fileInfos[i]);
+                }
+            }
+
+            SQLite3Data data = Resources.Load<SQLite3Data>("Sqlite3Data");
+            string dataPath = AssetDatabase.GetAssetPath(data);
+            data = ScriptableObject.CreateInstance<SQLite3Data>();
+            int dbCount = dbFileInfos.Count;
+            data.AllData = new List<SQLite3SingleData>(dbCount);
+            for (int i = 0; i < dbCount; i++)
+            {
+                SQLite3SingleData singleData = new SQLite3SingleData
+                {
+                    Name = dbFileInfos[i].Name,
+                    LocalName = MD5Tools.GetStringMd5(dbFileInfos[i].Name),
+                    Md5 = MD5Tools.GetFileMd5(dbFileInfos[i].FullName)
+                };
+                string dirPath = dbFileInfos[i].DirectoryName;
+                singleData.Directory = string.IsNullOrEmpty(dirPath) || dirPath == streamingAssetsPath
+                    ? string.Empty
+                    : dirPath.Replace('\\', '/').Replace(streamingAssetsPath, string.Empty);
+
+                data.AllData.Add(singleData);
+            }
+
+            AssetDatabase.CreateAsset(data, dataPath);
+            AssetDatabase.SaveAssets();
         }
-        AssetDatabase.CreateAsset(data, dataPath);
-        AssetDatabase.SaveAssets();
     }
 }
